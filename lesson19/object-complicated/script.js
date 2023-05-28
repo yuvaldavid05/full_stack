f42a108657990d4452d867e242fbbaf514f884a0
Yuval <abc@example.com>
2023-05-28 16:09:16 +0300

    added

---
+++ lesson19/object-complicated/script.js
@@ -0,0 +1,48 @@
+const numbers = [27,27,41,37,29,54,6,95,22,63,33,34,68,42,99,61,46,41,79,9,59,59,49,17,8,31,49,70,31,71];
+
+const tasks = [
+    {
+        title: 'יש להציג את סכום המערך',
+        result: numbers.reduce((res, n) => res += n, 0),
+    },
+    {
+        title: 'יש להציג את ממוצע המערך',
+        result: Math.round(numbers.reduce((res, n) => res += n, 0) / numbers.length),
+
+    },
+    {
+        title: 'יש להציג את המספר הגדול שבמערך',
+        result: Math.max(...numbers),
+    },
+    {
+        title: 'יש להציג את כל המספרים הגדולים מ-40',
+        result: numbers.filter(n => n > 40).join(', '),
+    }
+];
+
+const frameElem = document.querySelector('.frame');
+
+// הצגת המערך
+const p = document.createElement('p');
+p.className = 'alert';
+p.innerHTML = numbers.join(', ');
+frameElem.appendChild(p);
+
+tasks.forEach(task => {
+    // יצירת מסגרת לכל משימה
+    const div = document.createElement('div');
+    div.className = 'task';
+
+    // כותרת של המשימה
+    const h2 = document.createElement('h2');
+    h2.innerHTML = task.title;
+    div.appendChild(h2);
+
+    // אלמנט שמציג את התוצאה
+    const p = document.createElement('p');
+    p.innerHTML = task.result;
+    div.appendChild(p);
+
+    // הוספת המשימה לאתר
+    frameElem.appendChild(div);
+});
--- lesson19/spacial-gallery/script.js
+++ lesson19/spacial-gallery/script.js
@@ -1,6 +1,7 @@
 const images = ["תמונה (1).jpg", "תמונה (2).jpg", "תמונה (3).jpg", "תמונה (4).jpg", "תמונה (5).jpg", "תמונה (6).jpg", "תמונה (7).jpg", "תמונה (8).jpg", "תמונה (9).jpg", "תמונה (10).jpg", "תמונה (11).jpg", "תמונה (12).jpg", "תמונה (13).jpg", "תמונה (14).jpg", "תמונה (15).jpg", "תמונה (16).jpg", "תמונה (17).jpg"];
 
 let currentImage = -1;
+let myInterval;
 
 function nextImage() {
     currentImage++;
@@ -11,13 +12,13 @@ function nextImage() {
 
     const img = document.querySelector('#gallery img');
     img.src = `images/${images[currentImage]}`;
+
+    startAuto();
 }
 
 nextImage();
 
-function prevImage(ev) {
-    ev.preventDefault();
-
+function prevImage() {
     currentImage--;
 
     if (currentImage < 0) {
@@ -26,4 +27,26 @@ function prevImage(ev) {
 
     const img = document.querySelector('#gallery img');
     img.src = `images/${images[currentImage]}`;
+
+    startAuto();
+}
+
+window.addEventListener('keydown', ev => {
+    if (ev.key == 'ArrowRight') {
+        prevImage();
+    } else if (ev.key == 'ArrowLeft') {
+        nextImage();
+    }
+});
+
+function startAuto() {
+    // קודם כל מנקים את האינטרבל הקודם
+    stopAuto();
+
+    // מתחילים אינטרבל חדש
+    myInterval = setInterval(nextImage, 3 * 1000);
+}
+
+function stopAuto() {
+    clearInterval(myInterval);
 }
