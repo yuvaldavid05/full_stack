13b28ba66289be7f83f19cb3fb15ed08c272ecc3
Yuval <abc@example.com>
2023-07-10 23:16:22 +0300

    added

---
+++ lesson33/hello-react/src/components/Range.js
@@ -0,0 +1,18 @@
+function Range(){
+
+    const value = props.value;
+    // const min = props.min;
+    // const max = props.max;
+
+    function something(){
+        if(props.change){
+            props.change(value);
+        }
+    }
+
+    return (
+       
+    )
+}
+
+export default Range;
--- lesson33/hello-react/src/components/Settings.js
+++ lesson33/hello-react/src/components/Settings.js
@@ -4,9 +4,19 @@ import './Settings.css';
 function Settings() {
     const [brightness, setBrightness] = useState(100);
 
+
     function changeBrightness(val) {
         setBrightness(val);
         document.querySelector('html').style.filter = `brightness(${val}%)`;
+    }
+
+
+    const [fontSize, setFontSize] = useState(20);
+
+    function changeFontSize(valF) {
+        setFontSize(valF);
+        document.querySelector('.frame').style.fontSize = `${valF} + px`;
+        
     }
 
     return (
@@ -20,6 +30,9 @@ function Settings() {
             </div>
 
             <div className="card">
+                <b>גודל גופן:</b>
+                <input type="range" min="20" max="100" value={fontSize} onChange={ev => changeFontSize(+ev.target.value)}/>
+                <input type="number" min="20" max="100" value={fontSize} onChange={ev => changeFontSize(+ev.target.value)}/>
                 
             </div>
         </>
