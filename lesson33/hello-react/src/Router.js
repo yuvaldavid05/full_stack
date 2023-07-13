b7e7f1b26c6a0883799750ade916432aab7b8d9f
Yuval <abc@example.com>
2023-07-13 15:47:35 +0300

    added

--- lesson33/hello-react/src/Router.js
+++ lesson33/hello-react/src/Router.js
@@ -3,6 +3,9 @@ import Home from './components/Home';
 import Counter from './components/Counter';
 import Gallery from './components/Gallery';
 import Settings from './components/Settings';
+import ErrorPage from './components/ErrorPage';
+import Users from './components/Users';
+import UsersCards from './components/UsersCards';
 
 function Router(){
     return (
@@ -11,6 +14,8 @@ function Router(){
             <Route path="/counter" element={<Counter />} />
             <Route path="/gallery" element={<Gallery />} />
             <Route path="/settings" element={<Settings />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="*" element={<ErrorPage />} />
         </Routes>
     );
 }
---
+++ lesson33/hello-react/src/components/ErrorPage.js
@@ -0,0 +1,11 @@
+import './ErrorPage.css';
+
+export default function ErrorPage() {
+
+    return (
+        <div className='error'>
+            
+            <p>404</p>
+        </div>
+    );
+}
