0cb548dc49c23dc0df1a12e3891b4e6b8ca14fb6
Yuval <abc@example.com>
2023-07-30 16:33:34 +0300

    added

--- lesson33/news-manage/src/App.js
+++ lesson33/news-manage/src/App.js
@@ -1,15 +1,42 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Router from './Router';
+import Logout from './user/Logout';
+import RouterAuth from './RouterAuth';
+
+export const UserContext = React.createContext();
 
 function App() {
-    return (
-        <div className="App">
-            <h1>ניהול כתבות</h1>
+    const [user, setUser] = useState();
+    const [isLogged, setIsLogged] = useState();
 
-            <div className="frame">
-                <Router />
+    useEffect(() => {
+        fetch("https://api.shipap.co.il/login", {
+            credentials: 'include',
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.status === 'success') {
+                    setUser(data.user);
+                    setIsLogged(true);
+                } else {
+                    setUser();
+                    setIsLogged(false);
+                }
+            });
+    }, []);
+
+    return (
+        <UserContext.Provider value={{ user, setUser, isLogged, setIsLogged }}>
+            <div className="App">
+                <h1>ניהול כתבות</h1>
+
+                <div className="frame">
+                    {isLogged ? <Logout /> : ''}
+                    {isLogged ? <Router /> : <RouterAuth />}
+                </div>
             </div>
-        </div>
+        </UserContext.Provider>
     );
 }
 
--- lesson33/news-manage/src/user/Login.js
+++ lesson33/news-manage/src/user/Login.js
@@ -1,5 +1,110 @@
+import { Link, useNavigate } from 'react-router-dom';
 import './User.css';
+import { useContext, useEffect, useState } from 'react';
+import { JOI_HEBREW } from '../joi-hebrew';
+import Joi from 'joi';
+import { UserContext } from '../App';
 
 export default function Login() {
-    return <p>Login</p>
+    const [formData, setFormData] = useState({
+        userName: '',
+        password: '',
+    });
+    const [loginError, setLoginError] = useState('');
+    const [errors, setErrors] = useState({});
+    const [isValid, setIsValid] = useState(false);
+    const loginSchema = Joi.object({
+        userName: Joi.string().min(3).max(10).required(),
+        password: Joi.string().required(),
+    });
+    const { setUser, isLogged, setIsLogged } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isLogged) {
+            navigate('/');
+        }
+    }, [isLogged])
+
+    const login = ev => {
+        ev.preventDefault();
+
+        fetch("https://api.shipap.co.il/login", {
+            credentials: 'include',
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(formData),
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.status === 'success') {
+                    setUser(data.user);
+                    setIsLogged(true);
+                } else {
+                    setLoginError(data.message);
+                }
+            });
+    }
+
+    const handleError = ev => {
+        const { id, value } = ev.target;
+
+        const obj = {
+            ...formData,
+            [id]: value,
+        };
+
+        setFormData(obj);
+
+        const schema = loginSchema.validate(obj, { abortEarly: false, messages: { he: JOI_HEBREW }, errors: { language: 'he' } });
+        const errors = {};
+
+        if (schema.error) {
+            const error = schema.error.details.find(e => e.context.key === id);
+
+            if (error) {
+                errors[id] = error.message;
+            }
+
+            setIsValid(false);
+        } else {
+            setIsValid(true);
+        }
+
+        setErrors(errors);
+    }
+
+    return (
+        <>
+            <div className="Login smallFrame">
+                <h2>התחברות</h2>
+
+                <form onSubmit={login}>
+                    <label>
+                        שם משתמש:
+                        <input type="text" id='userName' value={formData.userName} className={errors.userName ? 'fieldError' : ''} onChange={handleError} />
+                    </label>
+
+                    {errors.userName ? <div className='fieldError'>{errors.userName}</div> : ''}
+
+                    <label>
+                        סיסמה:
+                        <input type="password" id='password' value={formData.password} className={errors.password ? 'fieldError' : ''} onChange={handleError} />
+                    </label>
+
+                    {errors.password ? <div className='fieldError'>{errors.password}</div> : ''}
+
+                    <button disabled={!isValid}>התחבר</button>
+
+                    {loginError ? <div className='fieldError'>{loginError}</div> : ''}
+                </form>
+            </div>
+
+            <p className="signup">
+                <Link to="/signup">להרשמה לחץ כאן</Link>
+            </p>
+        </>
+    )
 }
