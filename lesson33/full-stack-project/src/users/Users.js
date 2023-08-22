70c3b240428597f9556d994c06fbc5af20f8b698
Yuval <abc@example.com>
2023-08-22 12:13:12 +0300

    added

--- lesson33/full-stack-project/src/users/Users.js
+++ lesson33/full-stack-project/src/users/Users.js
@@ -1,46 +1,56 @@
 import './Users.css';
 import { useEffect, useState } from 'react';
-
+import moment from 'moment';
 
 export default function Users() {
     const [users, setUsers] = useState([]);
 
+    // useEffect(() => {
+    //     fetch("http://localhost:4000/users")
+    //     .then(res => res.json())
+    //     .then(data => setUsers(data.slice(0, 50)));
+    // }, []);
+
+    async function getUsers() {
+        const res = await fetch("http://localhost:4000/users");
+        const data = await res.json();
+        setUsers(data.slice(0, 50));
+    }
+
     useEffect(() => {
-        fetch("http://localhost:4000/users", {
-            credentials: 'include',
-        })
-            .then(res => res.json())
-            .then(data => setUsers(data));
-    }, [])
+        getUsers();
+    }, []);
 
     return (
         <div>
             <h2>משתמשים</h2>
+
             <table>
                 <thead>
                     <tr>
-                        <td>#</td>
-                        <td>שם מלא</td>
-                        <td>טלפון</td>
-                        <td>אימייל</td>
-                        <td>שם משתמש</td>
+                        <th>#</th>
+                        <th>תאריך יצירה</th>
+                        <th>שם פרטי</th>
+                        <th>שם משפחה</th>
+                        <th>אימייל</th>
+                        <th>טלפון</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        users.map((u, i) => {
+                        users.map((u, i) =>
                             <tr key={u.id}>
                                 <td>{i + 1}</td>
-                                <td>{u.firstName} {u.lastName}</td>
-                                <td>{u.phone}</td>
+                                <td>{moment(u.createdTime).format("DD/MM/YY")}</td>
+                                <td>{u.firstName}</td>
+                                <td>{u.lastName}</td>
                                 <td>{u.email}</td>
-                                <td>{u.username}</td>
+                                <td>{u.phone}</td>
                             </tr>
-                        })
+                        )
                     }
                 </tbody>
             </table>
-
         </div>
     )
 }
--- nodejs/hello-nodejs/router.js
+++ nodejs/hello-nodejs/router.js
@@ -2,16 +2,9 @@ const { getUsers, getUser } = require('./handlers/users');
 const { getClients, getClient } = require('./handlers/clients');
 
 module.exports = (app) => {
-
-    app.get('/', (req, res) => {
-        res.send({
-            message: "ברוכים הבאים",
-        });
-    });
-
-
-    app.get('/users', users.getUsers);
-    app.get('/users/:id', users.getUser);
-    app.get('/clients', clients.getClients);
-    app.get('/clients/:id', clients.getClient);
+    app.get('/', (req, res) => res.send('ברוכים הבאים'));
+    app.get('/users', getUsers);
+    app.get('/users/:id', getUser);
+    app.get('/clients', getClients);
+    app.get('/clients/:id', getClient);
 }
