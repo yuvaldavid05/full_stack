cb3ba8e952e67cd9e9563d203e833920e397a4ac
Yuval <abc@example.com>
2023-08-29 00:49:54 +0300

    added

--- lesson33/full-stack-project/src/Home.js
+++ lesson33/full-stack-project/src/Home.js
@@ -1,3 +1,17 @@
+import { useEffect, useState } from "react"
+
 export default function Home() {
-    return <h3>ברוכים הבאים</h3>
+    const [grades, setGrades] = useState([]);
+    const [sum, setSum] = useState(0);
+
+    useEffect(() => {
+        fetch("http://localhost:4000/grades")
+            .then(res => res.json())
+            .then(data => setGrades(data));
+    }, [])
+
+    return (
+        <h3>ברוכים הבאים</h3>
+
+    )
 }
