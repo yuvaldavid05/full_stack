596d893094d5780159a2761d4c0daf4cf7405712
Yuval <abc@example.com>
2023-09-08 19:23:07 +0300

    added

---
+++ nodejs/file-transfer/backend/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require('cors');
+const formidable = require('formidable');
+const fs = require('fs');
+const app = express();
+
+app.use(cors({
+    origin: true,
+    credentials: true,
+    methods: 'GET,PUT,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+}));
+
+app.listen(421);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.post('/files/upload', (req, res) => {
+    const form = new formidable.IncomingForm();
+
+    form.parse(req, (err, fields, files) => {
+        const file = files.myFile.pop();
+
+        fs.copyFile(file.filepath, `./files/${file.originalFilename}`, err => {
+            if (err) {
+                console.log(err);
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(`<meta charset="UTF-8">`);
+            res.write(`<h1>התמונה עלתה בהצלחה</h1>`);
+            res.write(`<img src="http://localhost:421/file/${file.originalFilename}" width="100%">`);
+            res.end();
+        });
+    });
+});
+
+app.get('/file/:fileName', (req, res) => {
+    res.sendFile(`${__dirname}/files/${req.params.fileName}`);
+});
+
+app.get('/files', (req, res) => {
+    fs.readdir(`${__dirname}/files`, (err, files) => {
+        if (err) {
+            throw err;
+        }
+
+        res.send(files);
+    });
+});
---
+++ nodejs/file-transfer/frontend/script.js
@@ -0,0 +1,31 @@
+function imageChange(ev) {
+    const file = ev.target.files[0];
+    const allowed = ['image/jpg', 'image/jpeg', 'image/png'];
+
+    if (!allowed.includes(file.type)) {
+        alert("קובץ לא מורשה");
+        return;
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = e => {
+        const img = document.querySelector('img');
+        img.src = e.target.result;
+        img.style.display = 'block';
+    }
+
+    reader.readAsDataURL(file);
+}
+
+function getAllImages() {
+    fetch("http://localhost:421/files")
+        .then(res => res.json())
+        .then(files => {
+            files.forEach(x => {
+                const img = document.createElement('img');
+                img.src = `http://localhost:421/file/${x}`;
+                document.querySelector('.gallery').appendChild(img);
+            });
+        });
+}
